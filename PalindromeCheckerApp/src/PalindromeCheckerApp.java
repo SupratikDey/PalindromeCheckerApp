1964741c45557edc8742ac5eb6e72f53a8c5a51a
saisuryapati <pp5488@srmist.edu.in>
2026-02-23 09:16:18 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,20 +1,39 @@
-/*
-    ========================================================================
-    MAIN CLASS - PalindromeCheckerApp
-    Usage/Goal - to take user input and tell if it is palindrome or not
-    ========================================================================
-    USE CASE 1
-    1) Understanding Git and Github and setting up on system
-    2)Printing out welcome message , version and initialization status.
-    3) Understanding ROOT branch and pushing to repositery
-    ========================================================================
-    @author:Supratik(developer)
-    @Version : 1.0
+/**
+ *
+ *MAIN CLASS Use Case 2Palindrome Checker App
+ * Use Case 2: Hardcoded Palindrome Validation
+ * Description:
+ *This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
+ *
+ *At this stage, the application:
+ * - Stores a predefined string
+ Compares characters from both ends
+ *- Determines whether the string is a palindrome
+ *- Displays the result on the console
+ *
+ *This use case introduces fundamental comparison logic
+ * before using advanced data structures.
+ *
+ * @author Developer
+ * @version 2.0
+public class UseCase2PalindromeChecker App {
+/**
+ * Application entry point for UC2.
+ *
+ * @param args Command-line arguments
  */
 
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the palindrome checker management system\nVersion : 1.0\nSystem initialized successfully.");
+    public static void main(String[] args){
+        String word = "madam";
+        for(int i=0;i<word.length()/2;i++){
+            if(word.charAt(i)!=word.charAt(word.length()-1-i)){
+                System.out.println("Not Palindrome");
+                return;
+            }
+        }
+        System.out.println("Palindrome");
     }
 }
